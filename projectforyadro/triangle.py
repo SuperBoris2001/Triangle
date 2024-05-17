f9a4bf78ff8223ccc0e3fffdc2fc271c9119a06e
root <root@LAPTOP-N3R476DK>
2024-05-17 23:32:55 +0300

    I added bug-report, changed tests and solved founded problems of triangle

--- projectforyadro/triangle.py
+++ projectforyadro/triangle.py
@@ -7,11 +7,16 @@ class Triangle(Enum):
     NotATriangle = "Не треугольник"
 
 def triangle(a: int, b: int, c: int):
-    if (a == b) or (a == c):
+    if (a <= 0) or (b <= 0) or (c <= 0):
+        return Triangle.NotATriangle
+    elif (a == b) and (a == c):
         return Triangle.Equal
-    elif a == b:
-        return Triangle.Isoscale
-    elif (a <= b + c) or (b <= a + c):
+    elif (a == b) or (a == c) or (b == c):
+        if (a < b + c) and (b < a + c) and (c < a + b):
+            return Triangle.Isoscale
+        else:
+            return Triangle.NotATriangle
+    elif (a < b + c) and (b < a + c) and (c < a + b):
         return Triangle.Regular
     else:
         return Triangle.NotATriangle
--- tests/tests_for_yadro/test_triangle.py
+++ tests/tests_for_yadro/test_triangle.py
@@ -1,4 +1,5 @@
 from projectforyadro.triangle import Triangle, triangle
+import pytest
 
 # Равносторонний треугольник
 def test_equilateral_triangle():
@@ -6,38 +7,39 @@ def test_equilateral_triangle():
 
 # Равнобедренный треугольник
 def test_isosceles_triangle():
-    assert triangle(5, 5, 3) == Triangle.Isoscale
-    assert triangle(5, 3, 5) == Triangle.Isoscale
-    assert triangle(3, 5, 5) == Triangle.Isoscale
+    pytest.assume(triangle(5, 5, 3) == Triangle.Isoscale)
+    pytest.assume(triangle(5, 3, 5) == Triangle.Isoscale)
+    pytest.assume(triangle(3, 5, 5) == Triangle.Isoscale)
 
 # Обычный треугольник
 def test_regular_triangle():
-    assert triangle(3, 4, 5) == Triangle.Regular
-    assert triangle(4, 5, 3) == Triangle.Regular
-    assert triangle(5, 3, 4) == Triangle.Regular
+    pytest.assume(triangle(3, 4, 5) == Triangle.Regular)
+    pytest.assume(triangle(4, 5, 3) == Triangle.Regular)
+    pytest.assume(triangle(5, 3, 4) == Triangle.Regular)
 
 # Не треугольник
 def test_not_a_triangle():
-    assert triangle(1, 1, 10) == Triangle.NotATriangle
-    assert triangle(1, 10, 1) == Triangle.NotATriangle
-    assert triangle(10, 1, 1) == Triangle.NotATriangle
+    pytest.assume(triangle(1, 1, 10) == Triangle.NotATriangle)
+    pytest.assume(triangle(1, 10, 1) == Triangle.NotATriangle)
+    pytest.assume(triangle(10, 1, 1) == Triangle.NotATriangle)
 
 # Граничные случаи
 def test_edge_cases():
-    assert triangle(0, 0, 0) == Triangle.Equal
-    assert triangle(1, 1, 2) == Triangle.NotATriangle
-    assert triangle(5, 5, 10) == Triangle.NotATriangle
+    pytest.assume(triangle(0, 0, 0) == Triangle.NotATriangle)
+    pytest.assume(triangle(1, 1, 2) == Triangle.NotATriangle)
+    pytest.assume(triangle(1, 2, 1) == Triangle.NotATriangle)
+    pytest.assume(triangle(2, 1, 1) == Triangle.NotATriangle)
 
 # Негативные случаи
 def test_negative_values():
-    assert triangle(-1, 2, 3) == Triangle.NotATriangle
-    assert triangle(1, -2, 3) == Triangle.NotATriangle
-    assert triangle(1, 2, -3) == Triangle.NotATriangle
+    pytest.assume(triangle(-1, 2, 3) == Triangle.NotATriangle)
+    pytest.assume(triangle(1, -2, 3) == Triangle.NotATriangle)
+    pytest.assume(triangle(1, 2, -3) == Triangle.NotATriangle)
 
 # Большие значения
 def test_large_values():
-    assert triangle(1000000, 1000000, 1000000) == Triangle.Equal
-    assert triangle(1000000, 1000000, 1000001) == Triangle.Isoscale
-    assert triangle(1000000, 1000001, 1000000) == Triangle.Isoscale
-    assert triangle(1000001, 1000000, 1000000) == Triangle.Isoscale
-    assert triangle(1000000, 1000001, 1000002) == Triangle.Regular
+    pytest.assume(triangle(1000000, 1000000, 1000000) == Triangle.Equal)
+    pytest.assume(triangle(1000000, 1000000, 1000001) == Triangle.Isoscale)
+    pytest.assume(triangle(1000000, 1000001, 1000000) == Triangle.Isoscale)
+    pytest.assume(triangle(1000001, 1000000, 1000000) == Triangle.Isoscale)
+    pytest.assume(triangle(1000000, 1000001, 1000002) == Triangle.Regular)
